27412b47cccfdae4d575c07870c22294ec49b095
Martin Angelov <martin_angelov@epam.com>
2017-04-24 19:02:31 +0300

    add determinant calculator using fork-join pool

--- src/main/java/martin/determinantcalculator/App.java
+++ src/main/java/martin/determinantcalculator/App.java
@@ -1,13 +1,33 @@
 package martin.determinantcalculator;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws ParseException
     {
-        System.out.println( "Hello World!" );
+    	Options options = new Options();
+    	CommandLineParser parser = new DefaultParser();
+    	CommandLine cmd = parser.parse( options, args);
+
+    	int tasks = 4;
+
+    	if (cmd.hasOption('i')) {
+    		
+    	} else if (cmd.hasOption('n')) {
+    		
+    	}
+
+    	if (cmd.hasOption('t')) {
+    		tasks = Integer.valueOf(cmd.getOptionValue('t'));
+    	}
     }
 }
