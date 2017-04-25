f9663d366ca6fe3df0e38f6742618c8cbc9e46e7
Martin Angelov <martin_angelov@epam.com>
2017-04-25 19:18:22 +0300

    some work on general program flow

--- src/main/java/martin/determinantcalculator/ConstraintInfo.java
+++
@@ -1,5 +0,0 @@
-package martin.determinantcalculator;
-
-public class ConstraintInfo {
-
-}
---
+++ src/main/java/martin/determinantcalculator/MatrixUtils.java
@@ -0,0 +1,11 @@
+package martin.determinantcalculator;
+
+public class MatrixUtils {
+	public float[][] readMatrixFromFile(String file) {
+		return null;
+	}
+
+	public float[][] generateRandomMatrix(int n) {
+		return null;
+	}
+}
